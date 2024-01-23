2350d3de25ecbf44aa2b988a763a05916ffd63e7
David J. Yu <87633683+ddjnw1yu@users.noreply.github.com>
2024-01-23 14:51:05 +1300

    update gallery package

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,6 +1,11 @@
 <script setup>
-import Gallery from "vue3-component-gallery";
-import "vue3-component-gallery/dist/style.css";
+// import Gallery from "vue3-component-gallery";
+// import GalleryHelper from "vue3-component-gallery/src/mixins/GalleryHelpers.js";
+// import "vue3-component-gallery/dist/style.css";
+import Gallery from "@abi-software/gallery";
+import GalleryHelper from "@abi-software/gallery/src/mixins/GalleryHelpers";
+import "@abi-software/gallery/dist/style.css";
+console.log("🚀 ~ GalleryHelper:", GalleryHelper)
 
 const dataInput = [
   {
